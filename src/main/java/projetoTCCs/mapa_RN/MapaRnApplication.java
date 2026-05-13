60a5762fcb9a19750107983ff3a41003ece947b7
Lucas Moura <felipelucasmoura350@gmail.com>
2026-05-13 14:14:08 -0300

    Iniciando

---
+++ src/main/java/projetoTCCs/mapa_RN/MapaRnApplication.java
@@ -0,0 +1,13 @@
+package projetoTCCs.mapa_RN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaRnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaRnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projetoTCCs/mapa_RN/MapaRnApplicationTests.java
@@ -0,0 +1,13 @@
+package projetoTCCs.mapa_RN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaRnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
